4db8de0e69bb246763bf79f59eb7ab005a7f8cb2
TrungNV <trungnv@ttc-solutions.com>
2020-06-18 16:53:14 +0700

    add hello

--- app.py
+++ app.py
@@ -10,18 +10,16 @@ app = Flask(__name__)
 api = Api(app)
 bcrypt = Bcrypt(app)
 
-app.config['JWT_SECRET_KEY'] = 'myspace'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
-app.config['MYSQL_DATABASE_DB'] = 'myspace'
-app.config['MYSQL_DATABASE_Host'] = 'localhost'
-
-# app.config.from_envvar('ENV_FILE_LOCATION')
+app.config.from_envvar('ENV_FILE_LOCATION')
 # export ENV_FILE_LOCATION=./.env
 
 initialize_db(app)
 initialize_routes(api)
 jwt = JWTManager(app)
+
+@app.route("/")
+def hello():
+    return "Hello word"
 
 if __name__ == "__main__":
     app.run()
